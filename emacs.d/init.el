8106fa1e1b2b1208f8b742bbe317181dd62dfa7c
178inaba <magic.brain.evolution.7.0@gmail.com>
2015-07-24 18:50:27 +0900

    fix init.el to bashrc -> sh-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,7 @@
-; not backup
+;;; not backup
 (setq make-backup-files nil
       auto-save-default nil
       auto-save-list-file-prefix nil)
+
+;;; bashrc -> sh-mode
+(add-to-list 'auto-mode-alist '("bashrc\\'" . sh-mode))
