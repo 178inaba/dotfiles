4b8e2344055f593433484eb21716962d84ec7574
178inaba <magic.brain.evolution.7.0@gmail.com>
2015-07-25 06:30:59 +0900

    add emacs package manager

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,5 @@
+;;;; base settings
+
 ;;; not backup
 (setq make-backup-files nil
       auto-save-default nil
@@ -5,3 +7,38 @@
 
 ;;; bashrc -> sh-mode
 (add-to-list 'auto-mode-alist '("bashrc\\'" . sh-mode))
+
+;;;; install packages
+
+;;; package list
+(setq my-init-pkgs
+      '(
+	markdown-mode
+	))
+
+;;; init
+(package-initialize)
+
+;;; add repo
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+;;; package installed check
+(setq my-init-pkg-install nil)
+(dolist (pkg my-init-pkgs)
+  (unless (package-installed-p pkg)
+    (setq my-init-pkg-install t)
+    ))
+
+;;; install
+(when my-init-pkg-install
+  (package-refresh-contents)
+  (dolist (pkg my-init-pkgs) (package-install pkg)))
+
+;;;; install package settings
+
+;;; markdown-mode
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
