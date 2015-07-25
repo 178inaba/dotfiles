9fe21ae9c8cc1dfca1580c1331fa0a94ae03c3ab
178inaba <magic.brain.evolution.7.0@gmail.com>
2015-07-26 01:39:19 +0900

    fix indent to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,22 +2,22 @@
 
 # package manager for each OS
 case $OSTYPE in
-	# mac
-	darwin*) PKGMGR='brew';;
-	# linux
-	linux*)
-		if type yum >/dev/null 2>&1; then
-			PKGMGR='sudo yum'
-		elif type apt-get >/dev/null 2>&1; then
-			PKGMGR='apt-get'
-		fi
-		;;
+    # mac
+    darwin*) PKGMGR='brew';;
+    # linux
+    linux*)
+	if type yum >/dev/null 2>&1; then
+	    PKGMGR='sudo yum'
+	elif type apt-get >/dev/null 2>&1; then
+	    PKGMGR='apt-get'
+	fi
+	;;
 esac
 
 echo "your package manager is $PKGMGR"
 
 if ! type git >/dev/null 2>&1; then
-	$PKGMGR install git
+    $PKGMGR install git
 fi
 
 CURPWD=$(pwd)
@@ -28,19 +28,19 @@ DF=~/.dotfiles
 
 # clone or pull
 if [ -d $DF ]; then
-	cd $DF
-	git pull
+    cd $DF
+    git pull
 else
-	git clone https://github.com/178inaba/dotfiles.git .dotfiles
-	cd $DF
+    git clone https://github.com/178inaba/dotfiles.git .dotfiles
+    cd $DF
 fi
 
 # link files
 FILES=(bashrc emacs.d)
 for FILE in ${FILES[@]}
 do
-	[ ! -e ~/.$FILE.local ] && mv -v ~/.$FILE ~/.$FILE.local
-	ln -fnsv $DF/$FILE ~/.$FILE
+    [ ! -e ~/.$FILE.local ] && mv -v ~/.$FILE ~/.$FILE.local
+    ln -fnsv $DF/$FILE ~/.$FILE
 done
 
 # download
