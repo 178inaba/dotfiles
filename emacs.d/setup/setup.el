be0528a63a74dcb2945e1e31fb9605ec6ae8c42a
178inaba <magic.brain.evolution.7.0@gmail.com>
2015-07-28 00:28:41 +0900

    delete load package.el(version 23 or less)

--- emacs.d/setup/setup.el
+++ emacs.d/setup/setup.el
@@ -1,8 +1,3 @@
-;;;; load package.el(version 23 or less)
-(when (file-exists-p "~/.emacs.d/dl/package.el")
-  (add-to-list 'load-path "~/.emacs.d/dl")
-  (load "package.el"))
-
 ;;;; install packages
 
 ;;; package list
