89e112b4b1ff3e1e77da3779eab28b844c86f337
178inaba <magic.brain.evolution.7.0@gmail.com>
2015-08-05 11:57:38 +0900

    fix init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -12,8 +12,9 @@
       auto-save-default nil
       auto-save-list-file-prefix nil)
 
-;;; bashrc -> sh-mode
+;;; bashrc, .bashrc.local -> sh-mode
 (add-to-list 'auto-mode-alist '("bashrc\\'" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.bashrc\\.local\\'" . sh-mode))
 
 ;;;; install package settings
 
