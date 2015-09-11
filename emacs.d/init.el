0031ffeb6461a9a7c6a9c56e4ae944b6395dba2a
178inaba <178inaba@users.noreply.github.com>
2015-09-11 23:16:36 +0900

    add yaml-mode setting

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -44,6 +44,11 @@
 ;;; web-mode
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
+;;; yaml-mode
+(add-hook 'yaml-mode-hook
+		  '(lambda ()
+			 (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
 ;;;; other settings
 
 ;;; gocode
