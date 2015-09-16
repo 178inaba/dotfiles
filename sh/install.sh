613c50a1ed88c009d5d31271c5ea8c5990bfc593
178inaba <178inaba@users.noreply.github.com>
2015-09-16 11:39:07 +0900

    add git-xlsx-textconv

--- sh/install.sh
+++ sh/install.sh
@@ -22,6 +22,7 @@ go get -u -v golang.org/x/tools/cmd/goimports
 go get -u -v code.google.com/p/rog-go/exp/cmd/godef
 go get -u -v github.com/golang/lint/golint
 go get -u -v github.com/nsf/gocode
+go get -u -v github.com/tokuhirom/git-xlsx-textconv
 
 # emacs
 NOW_EMACS_VER=(`emacs --version`)
