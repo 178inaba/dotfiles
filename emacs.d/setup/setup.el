a39e1a675f75fef624daa3029b1e1f0529b5173c
178inaba <178inaba@users.noreply.github.com>
2016-06-15 11:56:39 +0900

    fix indent to emacs setup

--- emacs.d/setup/setup.el
+++ emacs.d/setup/setup.el
@@ -2,46 +2,41 @@
 
 ;;; package list
 (setq my-init-pkgs
-	  '(
-		auto-complete
-		coffee-mode
-		dockerfile-mode
-		flycheck
-		gitconfig-mode
-		go-eldoc
-		go-mode
-		js2-mode
-		json-mode
-		markdown-mode
-		nginx-mode
-		perl6-mode
+      '(
+        auto-complete
+        coffee-mode
+        dockerfile-mode
+        flycheck
+        gitconfig-mode
+        go-eldoc
+        go-mode
+        js2-mode
+        json-mode
+        markdown-mode
+        nginx-mode
+        perl6-mode
         php-completion
-		php-mode
-		sql-indent
-		toml-mode
-		web-mode
-		yaml-mode
-		))
+        php-mode
+        sql-indent
+        toml-mode
+        web-mode
+        yaml-mode
+        ))
 
 ;;; load package.el
 (require 'package)
 
 ;;; add repo
-(add-to-list 'package-archives
-			 '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-			 '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
 ;;; package installed check
 (setq my-init-pkg-install nil)
 (catch 'end
   (dolist (pkg my-init-pkgs)
-    (unless (package-installed-p pkg)
-      (setq my-init-pkg-install t)
-      (throw 'end t))))
+    (unless (package-installed-p pkg) (setq my-init-pkg-install t) (throw 'end t))))
 
 ;;; install
 (when my-init-pkg-install
-  (package-refresh-contents)
-  (dolist (pkg my-init-pkgs) (package-install pkg)))
+  (package-refresh-contents) (dolist (pkg my-init-pkgs) (package-install pkg)))
