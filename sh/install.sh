9c5cc7ea54fd69c7bc65a9c64374c655da36937c
178inaba <178inaba@users.noreply.github.com>
2016-06-20 16:44:09 +0900

    fix download elisp path

--- sh/install.sh
+++ sh/install.sh
@@ -53,7 +53,7 @@ mkdir -pv $DF/dl/{bin,emacs,sh}
 curl -fsSL -o $DF/dl/sh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -fsSL -o $DF/dl/sh/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 curl -fsSL -o $DF/dl/emacs/sgml-mode-patch.el https://gist.githubusercontent.com/178inaba/b143a6f4621e0fcb240cf27e07312eef/raw/db49ed9caaafcb08f16a0389787a1991cbd70727/sgml-mode-patch.el
-curl -fsSL -o $DF/dl/emacs/php-cs-fixer.el https://raw.githubusercontent.com/178inaba/php-cs-fixer/compile_require_cl/php-cs-fixer.el
+curl -fsSL -o $DF/dl/emacs/php-cs-fixer.el https://raw.githubusercontent.com/OVYA/php-cs-fixer/master/php-cs-fixer.el
 curl -fsSL -o $DF/dl/bin/diff-highlight https://raw.githubusercontent.com/git/git/master/contrib/diff-highlight/diff-highlight
 
 # download file settings
