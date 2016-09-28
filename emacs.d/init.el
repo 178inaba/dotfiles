f81a4a7a87f88e712625f5dd307c9db942248418
178inaba <178inaba@users.noreply.github.com>
2016-09-28 11:16:03 +0900

    Add erb.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -83,6 +83,7 @@
 ;;; web-mode
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 
 ;; set engine
